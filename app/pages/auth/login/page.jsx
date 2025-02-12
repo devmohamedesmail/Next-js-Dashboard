b4f86466d90735d7315c1253262f4f60fe1a309a
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2025-02-12 13:52:34 +0400

    Fix Some Error for Building

--- app/pages/auth/login/page.jsx
+++ app/pages/auth/login/page.jsx
@@ -1,31 +1,11 @@
 import React from 'react'
+import CustomLoginInput from '@/app/components/CustomLoginInput'
+import CustomInput from '@/app/components/CustomInput'
+
 
 export default function page() {
   return (
     <div className="container m-auto px-5 ">
-
-      <div className="h-screen  flex justify-center items-center">
-        <div className=" w-full md:w-1/3 shadow-lg rounded-xl p-4">
-           <h3 className='text-center mb-5 font-bold text-2xl'>Login</h3>
-          <CustomLoginInput icon={<MdEmail />} />
-          <CustomLoginInput icon={<IoKey />} />
-
-
-          <div className="text-center mt-10">
-            <CustomButton title='Login' />
-          </div>
-
-          <div className="text-center mt-10">
-            <Link href='/pages/auth/register'>Register</Link>
-          </div>
-
-
-
-
-
-        </div>
-      </div>
-
 
 
     </div>
